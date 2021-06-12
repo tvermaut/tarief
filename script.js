572642fafd9012f07c56fb74305ecd810a22ee06
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2021-06-12 21:21:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 class Tariefsoort{
-  constructor(naam){
+  constructor(naam, gebouwd){
     this.naam = naam;
-    this.namen = [];
+    this.gebouwd = null;
+    this.gg = {};
+  }
+}
+
+class Provincie{
+  constructor(code, naam){
+    this.code = code;
+    this.naam = naam;
   }
 }
 
 class Gemeente{
-  constructor(naam){
+  constructor(naam, code, provincie, status){
     this.naam = naam;
+    this.code = code;
+    this.provincie = new Provincie(provincie.code, provincie.naam);
+    this.status = status;
     this.tariefsoorten = [];
   }
 }
+
+ats = {};
+
+oat = null;
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200){
+    oat = JSON.parse(this.responseText);
+    for g in oat.results{
+      gemeente = new Gemeente(g.naam, g.status);
+      for t in g.tariefsoorten{
+        if(t.naam not in ats){ats[t.naam] = new Tariefsoort(t.naam, t.gebouwd);}
+        ts = ats[t.naam];
+        for tarief in t.tarieven{
+          for gg in tarief.oatGebruik.keys(){
+            if(not(gg in ts)){
+              ts[gg] = tarief.oatGebruik[gg];
+            }
+          }
+        }
+      }
+    }
+  }
+};
+xhttp.open("GET", "https://oat.hisgis.nl/oat-ws/rest/tarieven", true);
+xhttp.setRequestHeader("Content-type", "application/json");
+xhttp.send(null);
