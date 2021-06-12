70a98f8f25c6594dd10f12fb6909c16cd22730a6
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2021-06-12 20:50:05 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+class Tariefsoort{
+  constructor(naam){
+    this.naam = naam;
+    this.namen = [];
+  }
+}
+
+class Gemeente{
+  constructor(naam){
+    this.naam = naam;
+    this.tariefsoorten = [];
+  }
+}
