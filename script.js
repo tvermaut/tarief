e5de0614f6bb1e0178b43337be4ed4729a9352a0
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2021-06-13 06:08:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -26,27 +26,29 @@ class Gemeente{
 ats = {};
 
 oat = null;
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200){
-    oat = JSON.parse(this.responseText);
-    alert(oat.keys());
-    for g in oat.results{
-      gemeente = new Gemeente(g.naam, g.status);
-      for t in g.tariefsoorten{
-        if(t.naam not in ats){ats[t.naam] = new Tariefsoort(t.naam, t.gebouwd);}
-        ts = ats[t.naam];
-        for tarief in t.tarieven{
-          for gg in tarief.oatGebruik.keys(){
-            if(not(gg in ts)){
-              ts[gg] = tarief.oatGebruik[gg];
+body.onload() = function(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200){
+      oat = JSON.parse(this.responseText);
+      alert(oat.keys());
+      for g in oat.results{
+        gemeente = new Gemeente(g.naam, g.status);
+        for t in g.tariefsoorten{
+          if(t.naam not in ats){ats[t.naam] = new Tariefsoort(t.naam, t.gebouwd);}
+          ts = ats[t.naam];
+          for tarief in t.tarieven{
+            for gg in tarief.oatGebruik.keys(){
+              if(not(gg in ts)){
+                ts[gg] = tarief.oatGebruik[gg];
+              }
             }
           }
         }
       }
     }
-  }
-};
-xhttp.open("GET", "https://oat.hisgis.nl/oat-ws/rest/tarieven");
-xhttp.setRequestHeader("Content-type", "application/json");
-xhttp.send(null);
+  };
+  xhttp.open("GET", "https://oat.hisgis.nl/oat-ws/rest/tarieven");
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send(null);
+}
