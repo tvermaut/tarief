9e7333d5923f18e463cad0a74b018b15b232a828
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2021-06-13 07:59:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ ats = {};
 
 oat = null;
 window.onload = function(){
+  oat = null;
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200){
